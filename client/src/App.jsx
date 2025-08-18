fe415777c8f663a199646dcf2180fc35624b990f
KyleWhite22 <kewhite900@gmail.com>
2025-08-18 17:23:02 -0400

    made login the homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Personal from './pages/Personal';
-import Education from './pages/Education';
-import Work from './pages/Work';
 import GameAILogin from './pages/GameAILogin';
 import GameAI from './pages/GameAI';
 
@@ -12,11 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/gameAILogin" element={<GameAILogin />} />
+        <Route path="/" element={<GameAILogin />} />
         <Route path="/gameAI" element={<GameAI />} />
       </Routes>
     </Router>
